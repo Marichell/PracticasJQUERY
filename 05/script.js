d3f4652d2e923ad9f91cadcf603c5e037eb601c6
marichell <marichell.ml@gmail.com>
2023-02-25 16:57:47 -0600

    05. Seleccionar un elemento y animarlo para que se desvanezca

---
+++ 05/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    //se desvanece automaticamente 8s después de que carga la página
+    setTimeout(function(){
+        $('.circle').fadeOut(1000);
+    }, 800);
+});
